2ae85bedb3b9b97acc28aeae70dfe4026262b281
mojasart <fullgamercsgo@hotmail.com>
2024-10-23 17:54:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -461,6 +461,7 @@ app.get('/style-create.css', (req, res) => {
   });
 });
 // Iniciar o servidor
-app.listen(4000, () => {
-    console.log('Servidor rodando na porta 4000');
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
